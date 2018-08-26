119fdfc07b9a8501e49f094191e80fc8e79de2a2
longnguyen <nguyenbinhlong92@gmail.com>
2018-08-26 21:14:07 +0700

    WS-2018.1.4 <longnguyen@Longs-MacBook-Pro.local Overwrite remote https://NguyenBinhLong@bitbucket.org/eaglecsapp/bds2.git to local

--- config/server.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by android@3forcom on 10/8/2016.
- */
-
-var release = true;
-
-exports.DATABASE_NAME   = "bds_v1";
-exports.DATABASE_SERVER        = '127.0.0.1:27017';
--- routes/script.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-var ScriptController = require('../controller/ScriptController');
-
-router.get('/generate/url', ScriptController.generateUrl);
-
-
-module.exports = router;
