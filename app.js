3a7478aebcb72a643644a67df6837535da64e32d
HuyTD <huytd@pascaliaasia.com>
2018-08-28 16:42:58 +0700

    add log4js module

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ var Models = require('./models/');
 var cors = require('cors');
 var rootRouter = require('./routes/root');
 
+// config log4js
+var log4js = require('log4js');
+log4js.configure('./config/log4js.json');
+
 var app = express();
 
 
