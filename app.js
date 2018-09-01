195bd724f50b17a3799ae4ed4b4c34c66909fb9a
HuyTD <huytd@pascaliaasia.com>
2018-09-01 20:54:32 +0700

    update return error as json

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
 var rootRouter = require('./src/routes/root');
+var HTTP_CODE = require('./src/constants/http-code');
 
 
 // config log4js
@@ -26,7 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(require('./src/middlewares/CheckToken'));
+// app.use(require('./src/middlewares/CheckToken'));
 
 app.use('/', rootRouter);
 
@@ -37,13 +38,20 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
+  return res.json({
+    status: HTTP_CODE.ERROR,
+    message: [
+      err.message
+    ],
+    data: {}
+  });
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // res.locals.message = err.message;
+  // res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  // res.status(err.status || 500);
+  // res.render('error');
 });
 
 module.exports = app;
