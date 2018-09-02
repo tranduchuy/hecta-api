fe8eabfb06c226c3a76b38275027f1c393ccda0c
HuyTD <huytd@pascaliaasia.com>
2018-09-02 22:10:52 +0700

    middleware check role admin

---
+++ middlewares/CheckRoleAdmin.js
@@ -0,0 +1,19 @@
+/**
+ * Example:
+ * router.get('xxx', require('./middlewares/CheckRoleAdmin'), function(req, res, next) {...})
+ */
+
+
+
+module.exports = async function (req, res, next) {
+  
+  if (req.user.role != global.USER_ROLE_ADMIN) {
+    return res.json({
+      status: 0,
+      message: ['Permission denied'],
+      data: {}
+    });
+  }
+
+  return next();
+}
