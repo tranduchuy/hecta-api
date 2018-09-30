024b369239a490167818a8a52b1dc938346e2ebe
HuyTD <huytd@pascaliaasia.com>
2018-10-01 03:58:38 +0700

    Get list notify, mark as read, count unread notify

--- controller/NotifyController.js
+++ controller/NotifyController.js
@@ -6,149 +6,182 @@ const httpCode = require('../config/http-code');
 const requestUtil = require('../utils/RequestUtil');
 
 /**
- * 
- * @param {*} status 
+ *
+ * @param {*} status
  * @return {Boolean}
  */
 const isValidStatusForUpdating = (status) => {
-  // TODO: will update this list when new status of notify appeared
-  return [
-    global.STATUS.NOTIFY_NONE,
-    global.STATUS.NOTIFY_READ
-  ].indexOf(status) !== -1;
+    // TODO: will update this list when new status of notify appeared
+    return [
+        global.STATUS.NOTIFY_NONE,
+        global.STATUS.NOTIFY_READ
+    ].indexOf(status) !== -1;
 };
 
 
 /**
  * @description Create a notify from fromUser to toUser
- * @param {*} params {fromUserId, toUserId, title, content}
+ * @param {*} _params {fromUserId, toUserId, title, content}
  * @return {Object}
  */
 const createNotify = async (_params) => {
-  const params = {..._params};
-  logger.info('NotifyController::createNotify is called', params);
-  const newNotify = await new NotifyModel();
-  newNotify.fromUser = params.fromUserId ? new mongoose.Types.ObjectId(params.fromUserId) : null;
-  newNotify.toUser = new mongoose.Types.ObjectId(params.toUserId);
-  newNotify.status = global.STATUS.NOTIFY_NONE;
-  newNotify.title = params.title.toString().trim();
-  newNotify.content = params.content.toString().trim();
-  newNotify.createdTime = new Date();
-  newNotify.updatedTime = new Date();
-  newNotify.type = params.type;
-  newNotify.params = params.params;
-  await newNotify.save();
+    const params = {..._params};
+    logger.info('NotifyController::createNotify is called', params);
+    const newNotify = await new NotifyModel();
+    newNotify.fromUser = params.fromUserId ? new mongoose.Types.ObjectId(params.fromUserId) : null;
+    newNotify.toUser = new mongoose.Types.ObjectId(params.toUserId);
+    newNotify.status = global.STATUS.NOTIFY_NONE;
+    newNotify.title = params.title.toString().trim();
+    newNotify.content = params.content.toString().trim();
+    newNotify.createdTime = new Date();
+    newNotify.updatedTime = new Date();
+    newNotify.type = params.type;
+    newNotify.params = params.params;
+    await newNotify.save();
 
-  return newNotify;
+    return newNotify;
 };
 
 /**
- * 
+ *
  * @param {*} req body: {status, title, content}, params: {notifyId}
- * @param {*} res 
- * @param {*} next 
+ * @param {*} res
+ * @param {*} next
  */
 const updateNotify = async (req, res, next) => {
-  logger.info('NotifyController::updateNotify is called');
+    logger.info('NotifyController::updateNotify is called');
 
-  try {
-    const notify = await NotifyModel.findOne({ _id: req.params['notifyId'] });
+    try {
+        const notify = await NotifyModel.findOne({_id: req.params['notifyId']});
 
-    let { title, content, status } = req.body;
-    if (isNaN(status)) {
-      return res.json({
-        status: httpCode.BAD_REQUEST,
-        message: ['Status is not a number'],
-        data: {}
-      });
-    } else {
-      status = parseInt(status);
+        let {title, content, status} = req.body;
+        if (isNaN(status)) {
+            return res.json({
+                status: httpCode.BAD_REQUEST,
+                message: ['Status is not a number'],
+                data: {}
+            });
+        } else {
+            status = parseInt(status);
+        }
+
+        if (notify == null) {
+            return res.json({
+                status: httpCode.ERROR,
+                message: ['Notify not found'],
+                data: {}
+            });
+        }
+
+        if (title) {
+            notify.title = title.toString().trim();
+        }
+
+        if (content) {
+            notify.content = content.toString().trim();
+        }
+
+        if (status && isValidStatusForUpdating(status)) {
+            notify.status = status;
+        }
+
+        notify.updatedTime = new Date();
+        await notify.save();
+
+        return res.json({
+            status: httpCode.SUCCESS,
+            message: ['Update notify successfully'],
+            data: {}
+        });
+
+    } catch (e) {
+        logger.error('NotifyController::updateNotify:error', e);
+        return next(e);
     }
-
-    if (notify == null) {
-      return res.json({
-        status: httpCode.ERROR,
-        message: ['Notify not found'],
-        data: {}
-      });
-    }
-
-    if (title) {
-      notify.title = title.toString().trim();
-    }
-
-    if (content) {
-      notify.content = content.toString().trim();
-    }
-
-    if (status && isValidStatusForUpdating(status)) {
-      notify.status = status;
-    }
-
-    notify.updatedTime = new Date();
-    await notify.save();
-
-    return res.json({
-      status: httpCode.SUCCESS,
-      message: ['Update notify successfully'],
-      data: {}
-    });
-
-  } catch (e) {
-    logger.error('NotifyController::updateNotify:error', e);
-    return next(e);
-  }
 };
 
 /**
  * Api get list notify
  * @param {*} req body: {status, title, content}, params: {notifyId}
- * @param {*} res 
- * @param {*} next 
+ * @param {*} res
+ * @param {*} next
  */
 const getListNotifies = async (req, res, next) => {
-  logger.info('NotifyController::getListNotifies is called');
-  try {
-    const { page, limit } = requestUtil.extractPaginationCondition(req);
+    logger.info('NotifyController::getListNotifies is called');
+    try {
+        const {page, limit} = requestUtil.extractPaginationCondition(req);
+        const query = {
+            toUser: req.user._id
+        };
+
+        const total = await NotifyModel.estimatedDocumentCount(query);
+
+        let notifies = await NotifyModel
+            .find(query)
+            .sort({createdTime: -1})
+            .skip((page - 1) * limit)
+            .limit(limit)
+            .populate('fromUser');
+
+        return res.json({
+            status: httpCode.SUCCESS,
+            message: [],
+            data: {
+                meta: {
+                    total,
+                    page,
+                    limit,
+                    sortBy: 'createdTime'
+                },
+                entry: notifies
+            }
+        })
+
+    } catch (e) {
+        logger.error('NotifyController::getListNotifies::error', e);
+        return next(e);
+    }
+};
+
+const getUnReadCountOfUser = (userId, cb) => {
     const query = {
-      toUser: req.user._id
+        toUser: userId,
+        status: global.STATUS.NOTIFY_NONE
     };
 
-    console.log(query);
+    NotifyModel.countDocuments(query, cb);
+};
 
-    let countUnRead = await NotifyModel.count({
-      toUser: req.user._id,
-      status: global.STATUS.NOTIFY_NONE
-    });
+const countUnRead = async (req, res, next) => {
+    logger.info('NotifyController::countUnRead is called');
 
-    let notifies = await NotifyModel
-      .find(query)
-      .sort({createdTime: -1})
-      .skip((page - 1)  * limit)
-      .limit(limit);
-    
-    return res.json({
-      status: httpCode.SUCCESS,
-      message: [],
-      data: {
-        meta: {
-          page,
-          limit,
-          sortBy: 'createdTime',
-          unRead: countUnRead
-        },
-        entry: notifies
-      }
-    })
+    try {
+        getUnReadCountOfUser(req.user._id, (err, count) => {
+            if (err) {
+                logger.error('NotifyController::countUnRead::error', e);
+                return next(err);
+            }
 
-  } catch (e) {
-    logger.error('NotifyController::getListNotifies error', e);
-    return next(e);
-  }
+            return res.json({
+                status: httpCode.SUCCESS,
+                message: '',
+                data: {
+                    entry: [],
+                    meta: {
+                        unread: count
+                    }
+                }
+            })
+        });
+    } catch (e) {
+        logger.error('NotifyController::countUnRead::error', e);
+        return next(e);
+    }
 };
 
 module.exports = {
-  createNotify,
-  updateNotify,
-  getListNotifies
+    createNotify,
+    updateNotify,
+    getListNotifies,
+    countUnRead
 };
--- routes/notify.js
+++ routes/notify.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var NotifyController = require('../controller/NotifyController');
 
+router.get('/count-unread', NotifyController.countUnRead);
 router.get('', NotifyController.getListNotifies);
 router.put('/:notifyId', NotifyController.updateNotify);
 
