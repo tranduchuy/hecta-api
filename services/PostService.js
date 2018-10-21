be9a9c7efe2f452e15935852eab0956853fedd1c
HuyTD <huytd@pascaliaasia.com>
2018-10-21 11:51:46 +0700

    Update stage

--- services/PostService.js
+++ services/PostService.js
@@ -126,11 +126,123 @@ const generateStageQueryPostProject = (req) => {
 };
 
 const generateStageQueryPostBuy = (req) => {
-    return [];
+    const {createdByType, status, id} = req.query;
+    const pageCond = RequestUtils.extractPaginationCondition(req);
+
+    const stages = [
+        {
+            $match: {
+                "postType": global.POST_TYPE_BUY,
+            }
+        },
+        {
+            $lookup: {
+                from: "Buys",
+                localField: "contentId",
+                foreignField: "_id",
+                as: "buyInfo"
+            }
+        },
+        {
+            $unwind: "$buyInfo"
+        }
+    ];
+
+    // filter
+    const stageFilter = {};
+
+    if (createdByType && createdByType !== '0') {
+        stageFilter["buyInfo.createdByType"] = parseInt(createdByType);
+    }
+
+    if (status) {
+        stageFilter["buyInfo.status"] = parseInt(status);
+    }
+
+    if (id) {
+        stageFilter["buyInfo._id"] = id;
+    }
+
+    if (Object.keys(stageFilter).length !== 0) {
+        stages.push({
+            $match: stageFilter
+        });
+    }
+
+    // pagination
+    stages.push({
+        $facet: {
+            entries: [
+                {$skip: (pageCond.page - 1) * pageCond.limit},
+                {$limit: pageCond.limit},
+            ],
+            meta: [
+                {$group: {_id: null, totalItems: {$sum: 1}}},
+            ],
+        },
+    });
+
+    return stages;
 };
 
 const generateStageQueryPostSale = (req) => {
-    return [];
+    const {createdByType, status, id} = req.query;
+    const pageCond = RequestUtils.extractPaginationCondition(req);
+
+    const stages = [
+        {
+            $match: {
+                "postType": global.POST_TYPE_SALE,
+            }
+        },
+        {
+            $lookup: {
+                from: "Sales",
+                localField: "contentId",
+                foreignField: "_id",
+                as: "saleInfo"
+            }
+        },
+        {
+            $unwind: "$saleInfo"
+        }
+    ];
+
+    // filter
+    const stageFilter = {};
+
+    if (createdByType && createdByType !== '0') {
+        stageFilter["saleInfo.createdByType"] = parseInt(createdByType);
+    }
+
+    if (status) {
+        stageFilter["saleInfo.status"] = parseInt(status);
+    }
+
+    if (id) {
+        stageFilter["saleInfo._id"] = id;
+    }
+
+    if (Object.keys(stageFilter).length !== 0) {
+        stages.push({
+            $match: stageFilter
+        });
+    }
+
+    // pagination
+    stages.push({
+        $facet: {
+            entries: [
+                {$skip: (pageCond.page - 1) * pageCond.limit},
+                {$limit: pageCond.limit},
+            ],
+            meta: [
+                {$group: {_id: null, totalItems: {$sum: 1}}},
+            ],
+        },
+    });
+
+    return stages;
 };
 
 module.exports = {
