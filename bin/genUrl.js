4ae38bc3daf4f689483262ce454755c97b69d1d5
Long Nguyen <nguyenbinhlong92@gmail.com>
2018-11-02 09:53:18 +0700

    gen Url

---
+++ bin/genUrl.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+/**
+ * Module dependencies.
+ */
+
+var app = require('../app');
+var http = require('http');
+var db = require('../config/db');
+var log4js = require('log4js');
+log4js.configure('./config/log4js.json');
+const config = require('config');
+var ScriptController = require('../controller/ScriptController');
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+var port = 9000;
+app.set('port', port);
+
+var server = http.createServer(app);
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.on('listening', onListening);
+
+db(() => {
+    
+    console.log('Connect to mongodb successfully');
+    server.listen(port);
+    
+    ScriptController.generateUrl2();
+});
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+    console.log('Server is running on port ', port);
+}
