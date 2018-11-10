c8a2dedecd8ccad4575d129b495d12f472a0b654
HuyTD <huytd@pascaliaasia.com>
2018-11-10 09:49:39 +0700

    Demo test middleware CheckToken

---
+++ middlewares/CheckToken.spec.js
@@ -0,0 +1,26 @@
+require('../config/def');
+const CheckToken = require('./CheckToken');
+
+let flag = 0;
+let result = null;
+const res = {
+    json: (value) => {
+        result = value;
+    }
+};
+const nextFn = () => {
+    flag++;
+};
+
+describe('CheckTokenMiddleware', () => {
+    beforeEach(() => {
+        flag = 0;
+        result = null;
+    });
+
+    it('Should return invalid token when token undefined and url not ignore', () => {
+        CheckToken({path: 'demo1', headers: {}}, res, nextFn);
+        console.log(result);
+        expect(result).toEqual({status: 0, message: 'Invalid token', data: {}});
+    });
+});
