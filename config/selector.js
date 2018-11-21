41a549812f52a4af1aa4743e526bc71fd5aa71c5
HuyTD <huytd@pascaliaasia.com>
2018-11-21 13:27:46 +0700

    Update constant priceListValue and areaListValue

--- config/selector.js
+++ config/selector.js
@@ -1,56 +1,56 @@
 const priceLevelValue = [
     [
-        {value: -1, text: "Thỏa thuận", min: {value: null}, max: {value: null}},
-        {value: 1, text: "\u003c 500 triệu", min: {value: null}, max: {value: 5e8}},
-        {value: 2, text: "500 - 800 triệu", min: {value: 5e8}, max: {value: 8e8}},
-        {value: 3, text: "800 triệu - 1 tỷ", min: {value: 8e8}, max: {value: 1e9}},
-        {value: 4, text: "1 - 2 tỷ", min: {value: 1e9}, max: {value: 1e9}},
-        {value: 5, text: "2 - 3 tỷ", min: {value: 2e9}, max: {value: 3e9}},
-        {value: 6, text: "3 - 5 tỷ", min: {value: 3e9}, max: {value: 5e9}},
-        {value: 7, text: "5 - 7 tỷ", min: {value: 5e9}, max: {value: 7e9}},
-        {value: 8, text: "7 - 10 tỷ", min: {value: 7e9}, max: {value: 10e9}},
-        {value: 9, text: "10 - 20 tỷ", min: {value: 10e9}, max: {value: 20e9}},
-        {value: 10, text: "20 - 30 tỷ", min: {value: 20e9}, max: {value: 30e9}},
-        {value: 11, text: "\u003e 30 tỷ", min: {value: 30e9}, max: {value: null}}
+        {value: -1, text: "Thỏa thuận", min: null, max: null},
+        {value: 1, text: "\u003c 500 triệu", min: null, max: 5e8},
+        {value: 3, text: "800 triệu - 1 tỷ", min: 8e8, max: 1e9},
+        {value: 4, text: "1 - 2 tỷ", min: 1e9, max: 2e9},
+        {value: 5, text: "2 - 3 tỷ", min: 2e9, max: 3e9},
+        {value: 6, text: "3 - 5 tỷ", min: 3e9, max: 5e9},
+        {value: 7, text: "5 - 7 tỷ", min: 5e9, max: 7e9},
+        {value: 8, text: "7 - 10 tỷ", min: 7e9, max: 10e9},
+        {value: 9, text: "10 - 20 tỷ", min: 10e9, max: 20e9},
+        {value: 10, text: "20 - 30 tỷ", min: 20e9, max: 30e9},
+        {value: 11, text: "\u003e 30 tỷ", min: 30e9, max: null}
     ],
     [
-        {value: -1, text: "Thỏa thuận", min: {value: null}, max: {value: null}},
-        {value: 1, text: "\u003c 1 triệu", min: {value: null}, max: {value: 1e6}},
-        {value: 2, text: "1 - 3 triệu", min: {value: 1e6}, max: {value: 3e6}},
-        {value: 3, text: "3 - 5 triệu", min: {value: 3e6}, max: {value: 5e6}},
-        {value: 4, text: "5 - 10 triệu", min: {value: 5e6}, max: {value: 10e6}},
-        {value: 5, text: "10 - 40 triệu", min: {value: 10e6}, max: {value: 40e6}},
-        {value: 6, text: "40 - 70 triệu", min: {value: 40e6}, max: {value: 70e6}},
-        {value: 7, text: "70 - 100 triệu", min: {value: 70e6}, max: {value: 100e6}},
-        {value: 8, text: "\u003e 100 triệu", min: {value: 100e6}, max: {value: null}}
+        {value: -1, text: "Thỏa thuận", min: null, max: null},
+        {value: 1, text: "\u003c 1 triệu", min: null, max: 1e6},
+        {value: 2, text: "1 - 3 triệu", min: 1e6, max: 3e6},
+        {value: 3, text: "3 - 5 triệu", min: 3e6, max: 5e6},
+        {value: 4, text: "5 - 10 triệu", min: 5e6, max: 10e6},
+        {value: 5, text: "10 - 40 triệu", min: 10e6, max: 40e6},
+        {value: 6, text: "40 - 70 triệu", min: 40e6, max: 70e6},
+        {value: 7, text: "70 - 100 triệu", min: 70e6, max: 100e6},
+        {value: 8, text: "\u003e 100 triệu", min: 100e6, max: null}
     ]
 ];
 
-module.exports.cateListBuy = [{
-    "id": 400,
-    "name": "Nhà đất cần mua",
-    "children": [
-        {"id": 402, "name": "Mua căn hộ chung cư"},
-        {"id": 403, "name": "Mua nhà riêng"},
-        {"id": 404, "name": "Mua nhà biệt thự, liền kề"},
-        {"id": 405, "name": "Mua nhà mặt phố"},
-        {"id": 406, "name": "Mua đất nền dự án"},
-        {"id": 407, "name": "Mua đất"},
-        {"id": 408, "name": "Mua trang trại, khu nghỉ dưỡng"},
-        {"id": 409, "name": "Mua kho, nhà xưởng"},
-        {"id": 410, "name": "Mua loại bất động sản khác"}
-    ],
-    "prices": [
-        {"name": "Thỏa thuận", "id": "-1"},
-        {"name": "Trăm nghìn/tháng", "id": "1"},
-        {"name": "Triệu/tháng", "id": "2"},
-        {"name": "Trăm nghìn/m2/tháng", "id": "5"},
-        {"name": "Triệu/m2/tháng", "id": "6"},
-        {"name": "Nghìn/m2/tháng", "id": "7"}
-    ],
-    'priceLevelValue': priceLevelValue[0],
-    "brokerdomain": null
-},
+module.exports.cateListBuy = [
+    {
+        "id": 400,
+        "name": "Nhà đất cần mua",
+        "children": [
+            {"id": 402, "name": "Mua căn hộ chung cư"},
+            {"id": 403, "name": "Mua nhà riêng"},
+            {"id": 404, "name": "Mua nhà biệt thự, liền kề"},
+            {"id": 405, "name": "Mua nhà mặt phố"},
+            {"id": 406, "name": "Mua đất nền dự án"},
+            {"id": 407, "name": "Mua đất"},
+            {"id": 408, "name": "Mua trang trại, khu nghỉ dưỡng"},
+            {"id": 409, "name": "Mua kho, nhà xưởng"},
+            {"id": 410, "name": "Mua loại bất động sản khác"}
+        ],
+        "prices": [
+            {"name": "Thỏa thuận", "id": "-1"},
+            {"name": "Trăm nghìn/tháng", "id": "1"},
+            {"name": "Triệu/tháng", "id": "2"},
+            {"name": "Trăm nghìn/m2/tháng", "id": "5"},
+            {"name": "Triệu/m2/tháng", "id": "6"},
+            {"name": "Nghìn/m2/tháng", "id": "7"}
+        ],
+        'priceLevelValue': priceLevelValue[0],
+        "brokerdomain": null
+    },
     {
         "id": 401, "name": "Nhà đất cần thuê",
         "children": [
@@ -172,17 +172,17 @@ module.exports.unitPriceList = [113.636, 47.272, 30, 14, 275, 1.2, 0];
 module.exports.unitPriceListNew = [168.181, 68.181, 50, 27.272, 454.545, 1.727, 0];
 
 module.exports.areaListValue = [
-    {value: -1, text: "Chưa xác định", min: {value: null}, max: {value: null}},
-    {value: 1, text: "\u003c 30 m2", min: {value: null}, max: {value: 30}},
-    {value: 2, text: "30 - 50 m2", min: {value: 30}, max: {value: 50}},
-    {value: 3, text: "50 - 80 m2", min: {value: 50}, max: {value: 80}},
-    {value: 4, text: "80 - 100 m2", min: {value: 80}, max: {value: 100}},
-    {value: 5, text: "100 - 150 m2", min: {value: 100}, max: {value: 150}},
-    {value: 6, text: "150 - 200 m2", min: {value: 150}, max: {value: 200}},
-    {value: 7, text: "200 - 250 m2", min: {value: 200}, max: {value: 250}},
-    {value: 8, text: "250 - 300 m2", min: {value: 250}, max: {value: 300}},
-    {value: 9, text: "300 - 500 m2", min: {value: 300}, max: {value: 500}},
-    {value: 10, text: "\u003e 500 m2", min: {value: 500}, max: {value: null}}
+    {value: -1, text: "Chưa xác định", min: null, max: null},
+    {value: 1, text: "\u003c 30 m2", min: null, max: 30},
+    {value: 2, text: "30 - 50 m2", min: 30, max: 50},
+    {value: 3, text: "50 - 80 m2", min: 50, max: 80},
+    {value: 4, text: "80 - 100 m2", min: 80, max: 100},
+    {value: 5, text: "100 - 150 m2", min: 100, max: 150},
+    {value: 6, text: "150 - 200 m2", min: 150, max: 200},
+    {value: 7, text: "200 - 250 m2", min: 200, max: 250},
+    {value: 8, text: "250 - 300 m2", min: 250, max: 300},
+    {value: 9, text: "300 - 500 m2", min: 300, max: 500},
+    {value: 10, text: "\u003e 500 m2", min: 500, max: null}
 ];
 
 module.exports.cityListOTher1 = require('./selector.json');
