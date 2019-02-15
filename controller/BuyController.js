5435ad512ec7d4387d246299eaea865e6639314b
HuyTD <huytd@pascaliaasia.com>
2019-02-15 16:13:27 +0700

    Remove Image Model, Controllers

--- controller/BuyController.js
+++ controller/BuyController.js
@@ -1,6 +1,5 @@
 const PostModel = require('../models/PostModel');
 const BuyModel = require('../models/BuyModel');
-const TokenModel = require('../models/TokenModel');
 const TagModel = require('../models/TagModel');
 const urlSlug = require('url-slug');
 const mongoose = require('mongoose');
@@ -11,288 +10,288 @@ const StringService = require('../services/StringService');
 const HttpCode = require('../config/http-code');
 
 const BuyController = {
-    add: async function (req, res, next) {
-        logger.info('BuyController::add::called');
+  add: async function (req, res, next) {
+    logger.info('BuyController::add::called');
 
-        const {
-            title, description, keywordList, formality,
-            type, city, district, ward, street, project,
-            area, price, unit,
-            address, images, contactName, contactAddress,
-            contactPhone, contactMobile, contactEmail,
-            receiveMail, from, to, createdByType
-        } = req.body;
+    const {
+      title, description, keywordList, formality,
+      type, city, district, ward, street, project,
+      area, price, unit,
+      address, images, contactName, contactAddress,
+      contactPhone, contactMobile, contactEmail,
+      receiveMail, from, to, createdByType
+    } = req.body;
 
-        ImageService.postConfirmImage(images);
-        try {
-            if (StringService.isUndefinedOrNull(title) || title.length < 30 || title.length > 99) {
-                return next(new Error('Invalid title'));
-            }
+    ImageService.postConfirmImage(images);
+    try {
+      if (StringService.isUndefinedOrNull(title) || title.length < 30 || title.length > 99) {
+        return next(new Error('Invalid title'));
+      }
 
-            if (StringService.isNaN(formality)) {
-                return next(new Error('Invalid formality'));
-            }
+      if (StringService.isNaN(formality)) {
+        return next(new Error('Invalid formality'));
+      }
 
-            if (StringService.isNaN(from) || from <= 0) {
-                return next(new Error('Invalid from date'));
-            }
+      if (StringService.isNaN(from) || from <= 0) {
+        return next(new Error('Invalid from date'));
+      }
 
-            if (StringService.isNaN(to) || to <= from) {
-                return next(new Error('Invalid to date'));
-            }
+      if (StringService.isNaN(to) || to <= from) {
+        return next(new Error('Invalid to date'));
+      }
 
-            if (StringService.isUndefinedOrNull(type) || type.toString().length === 0) {
-                return next(new Error('Invalid type'));
-            }
+      if (StringService.isUndefinedOrNull(type) || type.toString().length === 0) {
+        return next(new Error('Invalid type'));
+      }
 
-            if (StringService.isUndefinedOrNull(city) || city.toString() === '') {
-                return next(new Error('Invalid city'));
-            }
+      if (StringService.isUndefinedOrNull(city) || city.toString() === '') {
+        return next(new Error('Invalid city'));
+      }
 
-            if (StringService.isNaN(district)) {
-                return next(new Error('Invalid district'));
-            }
+      if (StringService.isNaN(district)) {
+        return next(new Error('Invalid district'));
+      }
 
-            if (StringService.isUndefinedOrNull(description) || description.length < 30) {
-                return next(new Error('Invalid description'));
-            }
+      if (StringService.isUndefinedOrNull(description) || description.length < 30) {
+        return next(new Error('Invalid description'));
+      }
 
-            if (StringService.isUndefinedOrNull(contactMobile) || contactMobile.length < 8 || contactMobile.length > 11) {
-                return next(new Error('Invalid contactMobile'));
-            }
+      if (StringService.isUndefinedOrNull(contactMobile) || contactMobile.length < 8 || contactMobile.length > 11) {
+        return next(new Error('Invalid contactMobile'));
+      }
 
-            let buy = new BuyModel();
-            let post = new PostModel();
-            
-            post.user = req.user.id;
+      let buy = new BuyModel();
+      let post = new PostModel();
 
-            if (createdByType) {
-                const duplicateTitle = await BuyModel.findOne({title: req.body.title.trim()}).lean();
-                if (duplicateTitle) {
-                    return next(new Error('Crawling duplicate title'))
-                }
-            }
+      post.user = req.user.id;
 
-            buy.title = title;
-            buy.description = description;
-            buy.keywordList = keywordList;
-            buy.formality = formality;
-            buy.type = type;
-            buy.city = city;
-            buy.district = district;
-            buy.ward = ward;
-            buy.street = street;
-            buy.project = project;
-            buy.area = (area && (area > 0)) ? area : null;
-            buy.price = (price && (price > 0)) ? price : null;
-            buy.unit = unit;
-            buy.address = address;
-            buy.images = images;
-            buy.contactName = contactName;
-            buy.contactAddress = contactAddress;
-            buy.contactPhone = contactPhone;
-            buy.contactMobile = contactMobile;
-            buy.contactEmail = contactEmail;
-            buy.receiveMail = receiveMail;
-
-            if (createdByType) {
-                buy.createdByType = createdByType;
-                buy.status = global.STATUS.ACTIVE;
-            } else {
-                buy.createdByType = global.CREATED_BY.HAND;
-            }
-
-            buy = await buy.save();
-            let url = urlSlug(title);
-            const countDuplicate = await PostModel.countDocuments({url});
-            if (countDuplicate > 0) {
-                url = url + '-' + countDuplicate;
-            }
-
-            post.url = url;
-            post.postType = global.POST_TYPE_BUY;
-            post.formality = buy.formality;
-            post.type = buy.type;
-            post.contentId = new mongoose.Types.ObjectId(buy._id);
-            post.from = from;
-            post.to = to;
-
-            if (createdByType) {
-                post.status = global.STATUS.ACTIVE;
-                post.paymentStatus = global.STATUS.PAYMENT_PAID;
-            } else {
-                post.status = global.STATUS.PENDING_OR_WAIT_COMFIRM;
-                post.paymentStatus = global.STATUS.PAYMENT_FREE;
-            }
-
-            if (keywordList && keywordList.length > 0) {
-                for (let i = 0; i < keywordList.length; i++) {
-                    const key = keywordList[i];
-                    const slug = urlSlug(key);
-
-                    if (!slug) {
-                        continue;
-                    }
-
-                    let tag = await TagModel.findOne({
-                        status: global.STATUS.ACTIVE,
-                        slug
-                    }).lean();
-
-                    if (!tag) {
-                        tag = new TagModel({
-                            slug: slug,
-                            keyword: key,
-                        });
-
-                        tag = await tag.save();
-                    }
-
-                    post.tags.push(tag._id);
-                }
-            }
-
-            post = await post.save();
-            logger.info('BuyController::add::success');
-
-            return res.json({
-                status: HttpCode.SUCCESS,
-                data: post,
-                message: 'Success'
-            });
-        } catch (e) {
-            logger.error('BuyController::add::error', e);
-            return next(e);
+      if (createdByType) {
+        const duplicateTitle = await BuyModel.findOne({title: req.body.title.trim()}).lean();
+        if (duplicateTitle) {
+          return next(new Error('Crawling duplicate title'))
         }
-    },
+      }
 
-    update: async (req, res, next) => {
-        logger.info('BuyController::update::called');
+      buy.title = title;
+      buy.description = description;
+      buy.keywordList = keywordList;
+      buy.formality = formality;
+      buy.type = type;
+      buy.city = city;
+      buy.district = district;
+      buy.ward = ward;
+      buy.street = street;
+      buy.project = project;
+      buy.area = (area && (area > 0)) ? area : null;
+      buy.price = (price && (price > 0)) ? price : null;
+      buy.unit = unit;
+      buy.address = address;
+      buy.images = images;
+      buy.contactName = contactName;
+      buy.contactAddress = contactAddress;
+      buy.contactPhone = contactPhone;
+      buy.contactMobile = contactMobile;
+      buy.contactEmail = contactEmail;
+      buy.receiveMail = receiveMail;
 
-        try {
-            let id = req.params.id;
-            if (!id) {
-                return next(new Error('Invalid Id'));
-            }
+      if (createdByType) {
+        buy.createdByType = createdByType;
+        buy.status = global.STATUS.ACTIVE;
+      } else {
+        buy.createdByType = global.CREATED_BY.HAND;
+      }
 
-            let post = await PostModel.findOne({_id: id});
-            if (!post || post.postType !== global.POST_TYPE_BUY) {
-                return next(new Error('Post not found'));
-            }
+      buy = await buy.save();
+      let url = urlSlug(title);
+      const countDuplicate = await PostModel.countDocuments({url});
+      if (countDuplicate > 0) {
+        url = url + '-' + countDuplicate;
+      }
 
-            if (post.user.toString() !== req.user._id.toString()) {
-                return next(new Error('Permission denied'));
-            }
+      post.url = url;
+      post.postType = global.POST_TYPE_BUY;
+      post.formality = buy.formality;
+      post.type = buy.type;
+      post.contentId = new mongoose.Types.ObjectId(buy._id);
+      post.from = from;
+      post.to = to;
 
-            let buy = await BuyModel.findOne({_id: post.contentId});
-            if (!buy) {
-                return next(new Error('Buy not found'));
-            }
+      if (createdByType) {
+        post.status = global.STATUS.ACTIVE;
+        post.paymentStatus = global.STATUS.PAYMENT_PAID;
+      } else {
+        post.status = global.STATUS.PENDING_OR_WAIT_COMFIRM;
+        post.paymentStatus = global.STATUS.PAYMENT_FREE;
+      }
 
-            let {
-                title, description, keywordList, formality, type, city,
-                district, ward, street, project, area, price, unit,
-                address, images, contactName, contactAddress, contactPhone,
-                contactMobile, contactEmail, receiveMail, status, from, to
-            } = req.body;
+      if (keywordList && keywordList.length > 0) {
+        for (let i = 0; i < keywordList.length; i++) {
+          const key = keywordList[i];
+          const slug = urlSlug(key);
 
-            if (title && (buy.title !== title.toString().trim())) {
-                const trimmedTitle = title.toString().trim();
-                buy.title = trimmedTitle;
-                let url = urlSlug(trimmedTitle);
-                let countDuplicate = await PostModel.countDocuments({url: url});
-                if (countDuplicate > 0) {
-                    url = url + "-" + countDuplicate;
-                }
+          if (!slug) {
+            continue;
+          }
 
-                post.url = url;
-            }
+          let tag = await TagModel.findOne({
+            status: global.STATUS.ACTIVE,
+            slug
+          }).lean();
 
-            buy.description = description || buy.description;
-            buy.keywordList = keywordList || buy.keywordList;
-            buy.formality = formality || buy.formality;
-            buy.type = type || buy.type;
-            buy.city = city || buy.city;
-            buy.district = district || buy.district;
-            buy.ward = ward || buy.ward;
-            buy.street = street || buy.street;
-            buy.project = project || buy.project;
-            buy.area = area ? (area > 0 ? area : null) : buy.area;
-            buy.price = price ? (price > 0 ? price : null) : buy.price;
-            buy.unit = unit || buy.unit;
-            buy.address = address || buy.address;
-            buy.contactName = contactName || buy.contactName;
-            buy.contactAddress = contactAddress || buy.contactAddress;
-            buy.contactPhone = contactPhone || buy.contactPhone;
-            buy.contactMobile = contactMobile || buy.contactMobile;
-            buy.contactEmail = contactEmail || buy.contactEmail;
-            buy.receiveMail = receiveMail || buy.receiveMail;
-            buy.status = global.STATUS.PENDING_OR_WAIT_COMFIRM;
-
-            if (images) {
-                ImageService.putUpdateImage(buy.images, images);
-                buy.images = images;
-            }
-
-            post.formality = buy.formality;
-            post.type = buy.type;
-
-            if (from) {
-                post.from = from;
-                post.status = global.STATUS.PENDING_OR_WAIT_COMFIRM;
-                post.status = global.STATUS.PAYMENT_UNPAID;
-                post.refresh = Date.now();
-            }
-
-            if (to) {
-                post.to = to;
-                post.status = global.STATUS.PENDING_OR_WAIT_COMFIRM;
-                post.status = global.STATUS.PAYMENT_UNPAID;
-            }
-
-            if (status === global.STATUS.DELETE) {
-                post.status = status;
-                buy.status = status;
-            }
-
-            post.tags = [];
-
-            if (keywordList && keywordList.length > 0) {
-                for (let i = 0; i < keywordList.length; i++) {
-                    const keyword = keywordList[i];
-                    const slug = urlSlug(keyword);
-                    if (!slug) {
-                        continue;
-                    }
-
-                    let tag = await TagModel.findOne({slug});
-                    if (!tag) {
-                        tag = new TagModel({
-                            slug: slug,
-                            keyword,
-                        });
-
-                        await tag.save();
-                    }
-
-                    post.tags.push(tag._id);
-                }
-            }
-
-            await buy.save();
-            await post.save();
-            logger.info('BuyController::update::success');
-
-            return res.json({
-                status: HttpCode.SUCCESS,
-                data: {},
-                message: 'Success'
+          if (!tag) {
+            tag = new TagModel({
+              slug: slug,
+              keyword: key,
             });
-        } catch (e) {
-            logger.error('BuyController::update::error', e);
-            return next(e);
+
+            tag = await tag.save();
+          }
+
+          post.tags.push(tag._id);
         }
+      }
+
+      post = await post.save();
+      logger.info('BuyController::add::success');
+
+      return res.json({
+        status: HttpCode.SUCCESS,
+        data: post,
+        message: 'Success'
+      });
+    } catch (e) {
+      logger.error('BuyController::add::error', e);
+      return next(e);
     }
+  },
+
+  update: async (req, res, next) => {
+    logger.info('BuyController::update::called');
+
+    try {
+      let id = req.params.id;
+      if (!id) {
+        return next(new Error('Invalid Id'));
+      }
+
+      let post = await PostModel.findOne({_id: id});
+      if (!post || post.postType !== global.POST_TYPE_BUY) {
+        return next(new Error('Post not found'));
+      }
+
+      if (post.user.toString() !== req.user._id.toString()) {
+        return next(new Error('Permission denied'));
+      }
+
+      let buy = await BuyModel.findOne({_id: post.contentId});
+      if (!buy) {
+        return next(new Error('Buy not found'));
+      }
+
+      let {
+        title, description, keywordList, formality, type, city,
+        district, ward, street, project, area, price, unit,
+        address, images, contactName, contactAddress, contactPhone,
+        contactMobile, contactEmail, receiveMail, status, from, to
+      } = req.body;
+
+      if (title && (buy.title !== title.toString().trim())) {
+        const trimmedTitle = title.toString().trim();
+        buy.title = trimmedTitle;
+        let url = urlSlug(trimmedTitle);
+        let countDuplicate = await PostModel.countDocuments({url: url});
+        if (countDuplicate > 0) {
+          url = url + '-' + countDuplicate;
+        }
+
+        post.url = url;
+      }
+
+      buy.description = description || buy.description;
+      buy.keywordList = keywordList || buy.keywordList;
+      buy.formality = formality || buy.formality;
+      buy.type = type || buy.type;
+      buy.city = city || buy.city;
+      buy.district = district || buy.district;
+      buy.ward = ward || buy.ward;
+      buy.street = street || buy.street;
+      buy.project = project || buy.project;
+      buy.area = area ? (area > 0 ? area : null) : buy.area;
+      buy.price = price ? (price > 0 ? price : null) : buy.price;
+      buy.unit = unit || buy.unit;
+      buy.address = address || buy.address;
+      buy.contactName = contactName || buy.contactName;
+      buy.contactAddress = contactAddress || buy.contactAddress;
+      buy.contactPhone = contactPhone || buy.contactPhone;
+      buy.contactMobile = contactMobile || buy.contactMobile;
+      buy.contactEmail = contactEmail || buy.contactEmail;
+      buy.receiveMail = receiveMail || buy.receiveMail;
+      buy.status = global.STATUS.PENDING_OR_WAIT_COMFIRM;
+
+      if (images) {
+        ImageService.putUpdateImage(buy.images, images);
+        buy.images = images;
+      }
+
+      post.formality = buy.formality;
+      post.type = buy.type;
+
+      if (from) {
+        post.from = from;
+        post.status = global.STATUS.PENDING_OR_WAIT_COMFIRM;
+        post.status = global.STATUS.PAYMENT_UNPAID;
+        post.refresh = Date.now();
+      }
+
+      if (to) {
+        post.to = to;
+        post.status = global.STATUS.PENDING_OR_WAIT_COMFIRM;
+        post.status = global.STATUS.PAYMENT_UNPAID;
+      }
+
+      if (status === global.STATUS.DELETE) {
+        post.status = status;
+        buy.status = status;
+      }
+
+      post.tags = [];
+
+      if (keywordList && keywordList.length > 0) {
+        for (let i = 0; i < keywordList.length; i++) {
+          const keyword = keywordList[i];
+          const slug = urlSlug(keyword);
+          if (!slug) {
+            continue;
+          }
+
+          let tag = await TagModel.findOne({slug});
+          if (!tag) {
+            tag = new TagModel({
+              slug: slug,
+              keyword,
+            });
+
+            await tag.save();
+          }
+
+          post.tags.push(tag._id);
+        }
+      }
+
+      await buy.save();
+      await post.save();
+      logger.info('BuyController::update::success');
+
+      return res.json({
+        status: HttpCode.SUCCESS,
+        data: {},
+        message: 'Success'
+      });
+    } catch (e) {
+      logger.error('BuyController::update::error', e);
+      return next(e);
+    }
+  }
 };
 
 module.exports = BuyController;
--- controller/CityController.js
+++ controller/CityController.js
@@ -6,82 +6,82 @@ const ProjectModel = require('../models/ProjectModel');
 const moment = require('moment');
 
 function compare(a, b) {
-    if (a.name < b.name)
-        return -1;
-    if (a.name > b.name)
-        return 1;
-    return 0;
+  if (a.name < b.name)
+    return -1;
+  if (a.name > b.name)
+    return 1;
+  return 0;
 }
 
 const getList = async (req, res, next) => {
-    logger.info('CityController::getList is called');
-    try {
-        let cities = await CityModel.aggregate([
-            {
-                $lookup: {
-                    from: "Districts",
-                    localField: "_id",
-                    foreignField: "city",
-                    as: "districts"
-                }
-            }
-        ]);
+  logger.info('CityController::getList is called');
+  try {
+    let cities = await CityModel.aggregate([
+      {
+        $lookup: {
+          from: 'Districts',
+          localField: '_id',
+          foreignField: 'city',
+          as: 'districts'
+        }
+      }
+    ]);
 
-        cities = await Promise.all(cities.map(async (city) => {
-            city['district'] = await Promise.all(city['districts'].map(async (district) => {
-                district.project = await ProjectModel
-                    .find({
-                        city: city.code,
-                        district: district.id
-                    })
-                    .select({
-                        _id: 1,
-                        id: 1,
-                        title: 1
-                    })
-                    .lean();
+    cities = await Promise.all(cities.map(async (city) => {
+      city['district'] = await Promise.all(city['districts'].map(async (district) => {
+        district.project = await ProjectModel
+          .find({
+            city: city.code,
+            district: district.id
+          })
+          .select({
+            _id: 1,
+            id: 1,
+            title: 1
+          })
+          .lean();
 
-                district.project.forEach((p) => {
-                   p.name = p.title;
-                   delete p.title;
-                });
-
-                district.project.sort(compare);
-                district.ward = district.wards;
-                district.street = district.streets;
-                delete district.wards;
-                delete district.streets;
-                delete district.city;
-                delete district.__v;
-
-                return district;
-            }));
-
-            city.district.sort(compare);
-            delete city['districts'];
-            delete city.__v;
-            delete city._id;
-            return city;
-        }));
-
-        cities.sort(compare);
-
-        const filePath = `${__dirname}/../public/files/selectors/cities-${moment().format('YYYY-MM-DD-hh-mm')}.json`;
-        fs.writeFile(filePath, JSON.stringify(cities), 'utf8', (err) => {
-            if (err) {
-                return next(err);
-            }
-
-            return res.download(filePath);
+        district.project.forEach((p) => {
+          p.name = p.title;
+          delete p.title;
         });
-    } catch (e) {
-        logger.error('CityController::getList::error', e);
-        return next(e);
-    }
+
+        district.project.sort(compare);
+        district.ward = district.wards;
+        district.street = district.streets;
+        delete district.wards;
+        delete district.streets;
+        delete district.city;
+        delete district.__v;
+
+        return district;
+      }));
+
+      city.district.sort(compare);
+      delete city['districts'];
+      delete city.__v;
+      delete city._id;
+      return city;
+    }));
+
+    cities.sort(compare);
+
+    const filePath = `${__dirname}/../public/files/selectors/cities-${moment().format('YYYY-MM-DD-hh-mm')}.json`;
+    fs.writeFile(filePath, JSON.stringify(cities), 'utf8', (err) => {
+      if (err) {
+        return next(err);
+      }
+
+      return res.download(filePath);
+    });
+  } catch (e) {
+    logger.error('CityController::getList::error', e);
+    return next(e);
+  }
 };
 
 const CityController = {
-    list: getList
+  list: getList
 };
 
 module.exports = CityController;
