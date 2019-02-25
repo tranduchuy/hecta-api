5f93b9d6e7f0dcf1e055062d3caf8dfc57ec751d
HuyTD <thewish.it@gmail.com>
2019-02-25 20:56:47 +0700

    Init model lead and leadHistory

--- models/LeadModel.js
+++ models/LeadModel.js
@@ -1,8 +1,37 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-// TODO
 const leadSchema = new Schema({
+  campaign: {
+    type: Schema.Types.ObjectId,
+    ref: 'Campaign'
+  },
+  user: {
+    type: Number,
+    default: null
+  },
+  status: {
+    type: Number,
+    default: global.STATUS.LEAD_NEW
+  },
+  isDeleted: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date
+  },
+  updatedAt: {
+    type: Date
+  },
+  referenceDomain: String,
+  utmSource: String,
+  utmCampaign: String,
+  utmMedium: String,
+  phone: String,
+  price: Number,
+  histories: [{type: Schema.Types.ObjectId, ref: 'LeadHistory'}],
+  note: String
 });
 
-const LeadModel = mongoose.model('Leads', leadSchema, 'Leads');
+const LeadModel = mongoose.model('Lead', leadSchema, 'Leads');
 module.exports = LeadModel;
