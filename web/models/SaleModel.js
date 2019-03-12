d678e736f7870da1d16156ec492d74878bd5b376
HuyTD <thewish.it@gmail.com>
2019-03-12 10:48:32 +0700

    UPdate SaleModel for saving analytic info

--- web/models/SaleModel.js
+++ web/models/SaleModel.js
@@ -4,40 +4,80 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const saleSchema = new Schema({
-    title: String,
-    formality: Number,
+  title: String,
+  formality: Number,
+  type: Number,
+  city: String,
+  district: Number,
+  ward: Number,
+  street: Number,
+  project: String,
+  area: Number,
+  areaData: Number,
+  price: Number,
+  priceData: Number,
+  unit: Number,
+  address: String,
+  keywordList: Array,
+  description: String,
+  streetWidth: Number,
+  frontSize: Number,
+  direction: Number,
+  balconyDirection: Number,
+  floorCount: Number,
+  bedroomCount: Number,
+  toiletCount: Number,
+  furniture: String,
+  images: Array,
+  contactName: String,
+  contactAddress: String,
+  contactPhone: String,
+  contactMobile: String,
+  contactEmail: String,
+  admin: {type: Array, default: []},
+  status: {type: Number, default: global.STATUS.PENDING_OR_WAIT_COMFIRM},
+  date: {type: Number, default: Date.now},
+  createdByType: {type: Number, default: global.CREATED_BY.HAND},
+  view: {
     type: Number,
-    city: String,
-    district: Number,
-    ward: Number,
-    street: Number,
-    project: String,
-    area: Number,
-    areaData: Number,
-    price: Number,
-    priceData: Number,
-    unit: Number,
-    address: String,
-    keywordList: Array,
-    description: String,
-    streetWidth: Number,
-    frontSize: Number,
-    direction: Number,
-    balconyDirection: Number,
-    floorCount: Number,
-    bedroomCount: Number,
-    toiletCount: Number,
-    furniture: String,
-    images: Array,
-    contactName: String,
-    contactAddress: String,
-    contactPhone: String,
-    contactMobile: String,
-    contactEmail: String,
-    admin: {type: Array, default: []},
-    status: {type: Number, default: global.STATUS.PENDING_OR_WAIT_COMFIRM},
-    date: {type: Number, default: Date.now},
-    createdByType: {type: Number, default: global.CREATED_BY.HAND}
+    default: 0
+  },
+  impression: {
+    type: Number,
+    default: 0,
+  },
+  click: {
+    type: Number,
+    default: 0
+  },
+  cpv: {
+    type: Number,
+    default: 0
+  },
+  ctr: {
+    type: Number,
+    default: 0
+  },
+  adRank: {
+    type: Number,
+    default: 0
+  },
+  paidForm: { // 1: thả theo ngày. 2: trả theo view
+    type: Number,
+    default: null
+  },
+  isValidBalance: { // TODO: chưa biết dùng khi nào, update khi nào. Check lại design
+    type: Boolean,
+    default: false
+  },
+  createdAt: {
+    type: Date,
+    default: new Date()
+  },
+  updatedAt: {
+    type: Date,
+    default: new Date()
+  }
 });
 
 const SaleModel = mongoose.model('Sale', saleSchema, 'Sales');
