f537fb3e94cfc4f392192c5ce0edff5a4b259c98
HuyTD <thewish.it@gmail.com>
2019-03-28 18:00:24 +0700

    Update 2 function in order to call rabbitmq

---
+++ web/services/RabbitMqService.js
@@ -0,0 +1,84 @@
+const amqp = require('amqplib/callback_api');
+const config = require('config');
+const rabbitMQConfig = config.get('rabbitmq');
+const RABBIT_MQ_NAMES = require('../config/rabbit-mq-channels');
+const uri = [
+  'amqp://',
+  rabbitMQConfig.username,
+  ':',
+  rabbitMQConfig.password,
+  '@',
+  rabbitMQConfig.host,
+  ':',
+  rabbitMQConfig.port
+].join('');
+
+const connectRabbitMQ = (queueName, cb) => {
+  console.log('rabbitMQ uri', uri);
+  amqp.connect(uri, function (err, conn) {
+    if (err) {
+      return cb(err);
+    }
+    console.log('Connect to RabbitMQ successfully');
+    conn.createChannel(function (err, ch) {
+      if (err) {
+        return cb(err);
+      }
+
+      ch.assertQueue(queueName, {durable: true});
+      return cb(null, ch, conn);
+    });
+  });
+};
+
+/**
+ *
+ * @param {string[]} saleIds
+ * @param {string} updateField
+ */
+const updateAdRank = (saleIds, updateField) => {
+  // updateField should be CLICK or IMPRESSION
+  connectRabbitMQ(RABBIT_MQ_NAMES.UPDATE_AD_RANK_OF_SALES, (err, channel, conn) => {
+    if (err) {
+      console.error(`Cannot connect queue ${RABBIT_MQ_NAMES.UPDATE_AD_RANK_OF_SALES}`, err);
+      return;
+    }
+
+    const message = {
+      saleIds,
+      updateField
+    };
+    channel.sendToQueue(RABBIT_MQ_NAMES.UPDATE_AD_RANK_OF_SALES, new Buffer(JSON.stringify(message)));
+    conn.close();
+  });
+};
+
+/**
+ *
+ * @param {string[]} saleIds
+ * @param {{}} logData
+ * @param {string} type
+ */
+const insertAdStatHistory = (saleIds, logData, type) => {
+  // type should be: VIEW or CLICK
+  connectRabbitMQ(RABBIT_MQ_NAMES.UPDATE_AD_RANK_OF_SALES, (err, channel, conn) => {
+    if (err) {
+      console.error(`Cannot connect queue ${RABBIT_MQ_NAMES.UPDATE_AD_RANK_OF_SALES}`, err);
+      return;
+    }
+
+    const message = {
+      saleIds,
+      logData,
+      type
+    };
+    channel.sendToQueue(RABBIT_MQ_NAMES.UPDATE_AD_RANK_OF_SALES, new Buffer(JSON.stringify(message)));
+    conn.close();
+  });
+};
+
+module.exports = {
+  connect: connectRabbitMQ,
+  updateAdRank,
+  insertAdStatHistory
+};
