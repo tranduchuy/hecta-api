8b855cc013c2e2d3941d50bb35e4235f90dae27e
HuyTD <thewish.it@gmail.com>
2019-03-30 18:52:35 +0700

    Fix bug

--- worker/app.js
+++ worker/app.js
@@ -4,6 +4,6 @@ const log4js = require('log4js');
 log4js.configure('./config/log4js.json');
 const app = express();
 
-require('./jobs/down-lead-price')();
-// require('./jobs/change-lead-status')();
+// require('./jobs/down-lead-price')();
+require('./jobs/change-lead-status')();
 module.exports = app;
