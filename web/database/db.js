63358805da5904ec695fe780ef08e14ef8489874
HuyTD <vincenthuytd@gmail.com>
2019-07-09 13:56:19 +0700

    Change config database to thuebds cloud mongo

--- web/database/db.js
+++ web/database/db.js
@@ -3,7 +3,7 @@ const mongoConfig = config.get('mongo');
 const mongoose = require('mongoose');
 
 module.exports = (callback) => {
-  const connectDbStr = `mongodb+srv://${mongoConfig.username}:${mongoConfig.password}@hecta-ye9xf.mongodb.net/hecta_v2?retryWrites=true`;
+  const connectDbStr = `mongodb+srv://${mongoConfig.username}:${mongoConfig.password}@cluster0-sffz7.gcp.mongodb.net/test?retryWrites=true&w=majority`;
 
   mongoose.connect(connectDbStr, {useNewUrlParser: true}, function (err) {
     if (err) {
